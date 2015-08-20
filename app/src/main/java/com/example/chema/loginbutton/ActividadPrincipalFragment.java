8437e5904834a7a4f26019817587f14875388666
chemicla1 <josemanuel.gomez.santos@gmail.com>
2015-08-20 20:23:21 +0200

    Primer commit: aqui esta el boton de FB creado hay funciones que no se bien bien que hacen, otras si, igualmente esta todo en la documentacion de developers.facebook

---
+++ app/src/main/java/com/example/chema/loginbutton/ActividadPrincipalFragment.java
@@ -0,0 +1,126 @@
+package com.example.chema.loginbutton;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ActividadPrincipalFragment extends Fragment {
+
+    private TextView mTextDetails;
+    private CallbackManager mCallBackManager;
+
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
+
+    private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            displayWelcomeMessage(profile);
+
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+
+        }
+    };
+
+    public ActividadPrincipalFragment() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        // Initialize the SDK before executing any other operations,
+        // especially, if you're using Facebook UI elements.
+        mCallBackManager = CallbackManager.Factory.create();
+         mTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken old, AccessToken newToken) {
+
+            }
+        };
+
+         mProfileTracker= new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                displayWelcomeMessage(newProfile);
+            }
+        };
+
+        mProfileTracker.startTracking();
+        mTokenTracker.startTracking();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_actividad_principal, container, false);
+    }
+
+    private void displayWelcomeMessage(Profile profile){
+        if(profile!=null){
+            mTextDetails.setText("Welcome " + profile.getName());
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        mTextDetails = (TextView) view.findViewById(R.id.text_details);
+
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setFragment(this);
+        loginButton.registerCallback(mCallBackManager,mCallback);
+
+    }
+
+    @Override
+    public void onResume() { //forma parte de la segunda parte del video
+        super.onResume();
+        Profile profile = Profile.getCurrentProfile();
+        displayWelcomeMessage(profile);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mTokenTracker.startTracking();
+        mProfileTracker.startTracking();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallBackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
